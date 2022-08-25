363e585f49518a1bd14c26b27872cb65a59dbc99
abonalumi <augustobonalumi@hotmail.com>
2022-08-25 20:05:15 -0300

    Controlador para modelo cerveza (beer)
    Se agrega controlador y vista para el modelo cerveza.
    El método index muestra un listado con las cervezas existentes para cada marca)

---
+++ ASPIntroNet6/ASPIntroWebAppMVC/Controllers/BeerController.cs
@@ -0,0 +1,20 @@
+﻿using ASPIntroWebAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPIntroWebAppMVC.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly PubContext _context;
+        public BeerController(PubContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var beers = await _context.Beers.Include(b => b.Brand).ToListAsync();
+            return View(beers);
+        }
+    }
+}
